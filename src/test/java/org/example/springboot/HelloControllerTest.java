023ac490998f591c7d41bd30b22179940b59a114
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2023-01-20 02:15:38 +0900

    HelloControllerTest에 추가된 api를 테스트하는 코드 추가

--- src/test/java/org/example/springboot/HelloControllerTest.java
+++ src/test/java/org/example/springboot/HelloControllerTest.java
@@ -28,4 +28,18 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect((content().string(hello)));
     }
+
+    @Test
+    public void helloDto_return() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name",name)
+                .param("amount", String.
+                        valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
+    }
 }
